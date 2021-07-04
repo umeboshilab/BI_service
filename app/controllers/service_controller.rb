75f10b5a29ec6ac7cbe6104b2658527108a8191a
taketake25 <takeru31415@gmail.com>
2021-07-04 19:37:10 +0900

    select code at local

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,5 +1,10 @@
 class ServiceController < ApplicationController
+<<<<<<< Updated upstream
     def index
         @requests = Request.all
     end
+=======
+  def index
+  end
+>>>>>>> Stashed changes
 end
