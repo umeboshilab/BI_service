66ce196a88ee639a2ae487c8e7eddbff4c2e98fe
taketake25 <takeru31415@gmail.com>
2021-07-10 16:38:42 +0900

    fix conflict

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,24 @@
+import { inputChangeSelector } from "@rails/ujs";
+import { ContextExclusionPlugin } from "webpack";
+
+function popupImage() {
+    var popup = document.getElementById('js-popup');
+    if (!popup) return;
+
+    var blackBg = document.getElementById('js-black-bg');
+    var closeBtn = document.getElementById('js-close-btn');
+    var showBtn = document.getElementById('js-show-popup');
+
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+        if (!elem) return;
+        elem.addEventListener('click', function () {
+            popup.classList.toggle('is-show');
+        });
+    }
+}
+popupImage();
+
+window.onload = function () { console.log("loaded") }
