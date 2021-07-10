950664d642adbbfdfb745302fbd529e47eaf2057
yito0424 <yito@mikilab.doshisha.ac.jp>
2021-07-10 21:57:44 +0900

    add acception and rejection functions

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -3,8 +3,6 @@ class ServiceController < ApplicationController
         @requests = Request.where(isChecked: 0)
         @request = Request.new(flash[:request])
         @tasks = Task.where(isAccepted: 1, isDone: 0)
-        @tasks.each do |task|
-            p task
-        end
+        @task = Task.new
     end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,33 @@
 class TasksController < ApplicationController
   def create
+    @task = Task.new(task_params)
+    error_msgs = []
+    ActiveRecord::Base.transaction do
+      if !@task.save
+        error_msgs << @task.errors.full_messages
+      end
+      if @task.request_id
+        if !@task.request.update({isChecked: true})
+          error_msgs << @task.request.errors.full_messages
+        end
+      else
+        error_msgs << 'Request_id is empty'
+      end
+      raise ActiveRecord::Rollback if error_msgs.present?
+    end
+    if error_msgs
+      redirect_to service_path, flash: {
+        task: @task,
+        error_messages: error_msgs.flatten
+      }
+    else
+      if @task.isAccepted = 1
+        flash[:notice] = "#{@task.request.title}を承認しました"
+      else
+        flash[:notice] = "#{@task.request.title}を拒否しました"
+      end
+      redirect_to service_path
+    end
   end
 
   def task
@@ -21,5 +49,9 @@ class TasksController < ApplicationController
   end
 
   def update
+  end
+
+  def task_params
+    params.require(:task).permit(:request_id, :host_user_id, :isAccepted, :comment)
   end
 end
