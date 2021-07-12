ada448e2e508fc0b6b28fabad90cd06814188237
taketake25 <takeru31415@gmail.com>
2021-07-13 08:36:26 +0900

    show popup to check requests by using js

--- app/javascript/packs/UI/index.js
+++ app/javascript/packs/UI/index.js
@@ -1,33 +1,70 @@
 document.addEventListener('turbolinks:load', () => {
 
-    function popupImage() {
-        var popup = document.getElementById('js-popup');
-        if (!popup) return;
+    function popupRequestCheckForm(e) {
+        var e = e || window.event;
+        var elem = e.currentTarget || e.srcElement;
+        var index = elem.getAttribute("data-index");
+        var title = elem.getAttribute("data-title");
+        var message = elem.getAttribute("data-message");
 
-        var blackBg = document.getElementById('js-black-bg');
-        var closeBtn = document.getElementById('js-close-btn');
-        var closeBtn2 = document.getElementById('js-close-btn2');
+        temp = document.getElementById('task_request_id');
+        temp.value = index;
+        document.getElementById('requestCheckFormTitle').innerHTML = title;
+        document.getElementById('requestCheckFormMessage').innerHTML = message;
 
-        console.log("hogehoge");
+        console.log(temp);
 
-        closePopUp(blackBg);
-        closePopUp(closeBtn);
-        closePopUp(closeBtn2);
-
-        function closePopUp(elem) {
-            if (!elem) return;
-            elem.addEventListener('click', function () {
-                popup.classList.toggle('is-show');
-            });
-        }
+        let checkForm = document.getElementById('popupRequestForm');
+        checkForm.classList.remove("hidden");
     }
 
-    var showBtn = document.getElementById('js-show-popup');
-    showBtn.onclick = popupImage;
+    function hideRequestCheckForm() {
+        let checkForm = document.getElementById('popupRequestForm');
 
-    // window.onload = function () {
-    //     console.log("loaded");
-    //     alert("hoge");
-    // }
+        checkForm.classList.add("hidden");
+    }
+
+    function acceptingRequestForm() {
+        console.log("accepted");
+        document.getElementById('task_isAccepted').value = '1';
+
+        // submitRequestCheckForm();
+        // hideRequestCheckForm();
+    }
+
+    function rejectingRequestForm() {
+        console.log("rejected");
+
+        document.getElementById('task_isAccepted').value = '0';
+
+        // submitRequestCheckForm();
+        // hideRequestCheckForm();
+    }
+
+    function submitRequestCheckForm() { //Ruby側で全部終えてありそう
+        var index = document.getElementById('task_request_id').value;
+        console.log(index);
+    }
+
+
+    // カードのクリックをpopupに対応させる
+    let requestCards = document.getElementsByClassName('requestCardButton');
+    for (var i = 0; i < requestCards.length; i++) {
+        requestCards[i].onclick = popupRequestCheckForm;
+    }
+
+
+    // let cardButton = document.getElementById('cardButton');
+    // cardButton.onclick = popupRequestCheckForm(1);
+
+    let hideCheckForm = document.getElementById('hideCheckPopup');
+    hideCheckForm.onclick = hideRequestCheckForm;
+
+    let acceptButton = document.getElementById('acceptButton');
+    acceptButton.onclick = acceptingRequestForm;
+
+    let rejectButton = document.getElementById('rejectButton');
+    rejectButton.onclick = rejectingRequestForm;
 
 })
+
