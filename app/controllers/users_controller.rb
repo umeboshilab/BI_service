a5b194793fb4e4802def1bd4b4058e047cfdeed8
yito0424 <yito@mikilab.doshisha.ac.jp>
2021-10-01 00:47:50 +0900

    add host user check box to sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,30 @@
 class UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to root_path
-    else
+    error_msgs = []
+    
+    ActiveRecord::Base.transaction do
+      if !user.save
+        error_msgs << user.errors.full_messages
+        raise ActiveRecord::Rollback
+      end
+      if params.require(:user).permit(:is_host)
+        host_user = HostUser.new({user_id: user.id, image: 'no_icon.png'})
+        if !host_user.save
+          error_msgs << host_user.errors.full_messages
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+
+    if error_msgs.present?
       redirect_to new_user_path, flash: {
         user: user,
-        error_messages: user.errors.full_messages
+        error_messages: error_msgs.flatten
       }
+    else
+      session[:user_id] = user.id
+      redirect_to root_path
     end
   end
 
